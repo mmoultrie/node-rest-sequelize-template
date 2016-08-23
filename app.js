204955009de85f9250abb0021e5d2b2152cd45ec
mmoultrie <mmoultrie@gmail.com>
2016-08-23 14:49:36 -0400

    Updated config file

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-
-var express = require('express');        
+var express = require('express');
 var app = express();                 
 var bodyParser = require('body-parser');
 var config = require('./config');
@@ -11,6 +10,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/api', router);
+
+//enable cors for dev
+app.use(function (req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
+	res.setHeader('Access-Control-Allow-Methods', '*');
+	res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
+	res.setHeader('Access-Control-Max-Age', '1000');
+	next();
+});
+
 
 models.sequelize.sync().then(function () {
 	app.listen(config.port);
--- config.js
+++ config.js
@@ -3,10 +3,21 @@ var db = {};
 db.user = '';
 db.password = '';
 db.uri = '';
-db.schema ='';
+db.schema ="";
 
+var opts = {
+    host: db.uri,
+    dialect: '' ,
+    define: {
+        //prevent sequelize from pluralizing table names
+        freezeTableName: true,
+        timestamps: false
+    }
+};
 
+config.secret= "mySecret";
 config.port = process.env.PORT || 9980;
 config.db = db;
+config.opts = opts;
 
 module.exports = config;
