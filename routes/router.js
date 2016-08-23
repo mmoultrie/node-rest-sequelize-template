a135c3fad7c2243ae435121edb6b25842b62013d
mmoultrie <mmoultrie@gmail.com>
2016-08-23 17:05:09 -0400

    Refactoring

--- routes/router.js
+++ routes/router.js
@@ -2,11 +2,6 @@ var express = require('express');
 var router = express.Router(); 
 var userRouter = require('./userroutes');
 
-router.get('/', function(req, res) {
-    res.type('text/plain');
-    res.send('testing');
-});
-
 router.use('/users',userRouter);
 
 module.exports = router;
--- routes/userroutes.js
+++ routes/userroutes.js
@@ -1,12 +1,35 @@
 var express = require('express');
-var userRouter = express.Router(); 
+var router = express.Router();
 var models = require('../models');
+var _model = models.User;
 
-userRouter.get('/', function(req, res) {
-    models.User.findAll().then(function(users) {
-        console.log(res);
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(users));
+// Api list get, post[field]
+
+//get all objs
+router.get('/', function(req, res) {
+    _model.findAll().then(function(objs) {
+        //res.setHeader('Content-Type', 'application/json');
+        res.send(objs);
     });
 });
-module.exports = userRouter;
+
+//post by an update obj
+router.put('/field', function(req, res) {
+    var obj = req.body;
+    var updateObject = function() {
+        var ob= {};
+        for(prop in obj){
+            if(prop != 'id'){
+                ob[prop] = obj[prop];
+            }
+        }
+        console.log(ob);
+        return ob;
+    };
+    _model.update(
+        updateObject(),
+        { where: { id: obj.id }
+        }).then( function() {
+    });
+});
+module.exports = router;
